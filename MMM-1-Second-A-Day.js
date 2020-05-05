c0c698b6f8b0680be5bcf7b235e6550b87c2f808
Gary Chew <garylchew@gmail.com>
2020-05-05 16:57:49 -0700

    Implement OnScreenMenu button for testing recording. (Recording doesn't work)

--- MMM-1-Second-A-Day.js
+++ MMM-1-Second-A-Day.js
@@ -16,5 +16,26 @@ Module.register('MMM-1-Second-A-Day',
 	start: function()
 	{
 	  Log.info('Starting module: ' + this.name);
+	  this.sendSocketNotification('START', this.config);
+	},
+
+    notificationReceived: function(notification, payload, sender) {
+        if (notification === "RECORD") {
+            this.sendSocketNotification(notification, payload);
+        }
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+	    Log.info("core socketNotif received");
+    },
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		var h = document.createElement("h1");
+		h.innerHTML = "hello test!"
+		wrapper.appendChild(h);
+
+		return wrapper;
 	}
 });
--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,44 @@
 'use strict';
 const NodeHelper = require('node_helper');
 
-module.exports = NodeHelper.create({});
+module.exports = NodeHelper.create({
+    start: function() {
+        console.log("Starting node helper for: " + this.name);
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "RECORD") {
+            Log.log("Received RECORD notification.");
+            this.record();
+        }
+    },
+
+    record : function() {
+        captureCamera(function(camera) {
+            var recorder = RecordRTC(camera, {
+                type: 'video'
+            });
+            Log.info("recording");
+            recorder.startRecording();
+
+            recorder.stopRecording(() => {
+                recorder.save('./video.webm');
+            });
+
+            // release camera on stopRecording
+            recorder.camera = camera;
+
+        });
+    },
+
+
+});
+
+function captureCamera(callback) {
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(function(camera) {
+        callback(camera);
+    }).catch(function(error) {
+        alert('Unable to capture your camera. Please check console logs.');
+        console.error(error);
+    });
+}
