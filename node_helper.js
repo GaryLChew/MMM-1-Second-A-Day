ec7e3e6461e2f4d14649a0db8a60485d30a4575c
Kyle Stadelmann <dlice@hotmail.com>
2020-05-12 23:09:23 -0700

    Integrated compile code

--- node_helper.js
+++ node_helper.js
@@ -11,6 +11,10 @@ module.exports = NodeHelper.create({
             console.log("Node helper received SAVE_1_SECOND_VIDEO");
             this.save1SecondVideo(payload);
         }
+        if (notification === "COMPILE_VIDEOS") {
+            console.log("Node helper received COMPILE_VIDEOS");
+            this.compileVideos();
+        }
     },
 
     save1SecondVideo : function(blob) {
@@ -28,4 +32,30 @@ module.exports = NodeHelper.create({
             console.log('video saved')
         })
     },
+
+    compileVideos: function () {
+		const ffmpeg = require('fluent-ffmpeg');
+		const fs = require('fs');
+
+		const PATH_TO_VIDEOS = './modules/MMM-1-Second-A-Day/videos/1-second-videos/';
+		const PATH_TO_COMPILATIONS ='./modules/MMM-1-Second-A-Day/videos/compilations/';
+
+		var command = ffmpeg();
+
+		filenames = fs.readdirSync(PATH_TO_VIDEOS);
+		filenames.forEach(function(filename) {
+  		// add each video file to ffmpeg command
+  			command.addInput(PATH_TO_VIDEOS + filename);
+		});
+
+		// call ffmpeg merge command
+		command
+  			.on('error', function(err) {
+    			console.log('An error occurred: ' + err.message);
+  			})
+  			.on('end', function() {
+    			console.log('Merging finished !');
+  			})
+  			.mergeToFile('merged.webm', PATH_TO_COMPILATIONS);
+	},
 });
